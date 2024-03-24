e53a04d19901a3f35ee81c797f7dcd2ef3ec6cfd
Amir Ghahremanian <amir0ghahremanian@outlook.com>
2024-03-24 23:50:40 +0330

    major improvement!

--- src/image.rs
+++ src/image.rs
@@ -1,7 +1,19 @@
+use std::{fs::OpenOptions, io::{self, Read, Write}};
+
+use serde::{Deserialize, Serialize};
+use toml::de::Error;
+
 pub struct Lab {
     image_path: String,
+    config: Option<LabConfig>,
     expanded_path: Option<String>,
     drive_letter: Option<String>,
+}
+
+#[derive(Serialize)]
+#[derive(Deserialize)]
+pub struct LabConfig {
+    name: String
 }
 
 pub struct LabBuilder {
@@ -29,14 +41,49 @@ impl Lab {
             image_path: path,
             expanded_path: None,
             drive_letter: None,
+            config: None,
         }
     }
 
-    pub fn expand(&mut self, path: String) -> Result<(), &'static str> {
-        Err("Not implemented!")
+    fn read_config(&mut self, path: &str) -> Result<(), String> {
+        let mut file = OpenOptions::new()
+            .read(true)
+            .open(path).str_result()?;
+
+        let config: LabConfig = {
+            let mut toml = String::new();
+
+            file.read_to_string(&mut toml).str_result()?;
+
+            toml::from_str(&toml).str_result()?
+        };
+
+        self.config = Some(config);
+
+        Ok(())
     }
 
-    pub fn mount(&mut self, drive_letter: String) -> Result<(), &'static str> {
+    fn write_config(&self, path: &str) -> Result<(), String> {
+        let mut file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(path).str_result()?;
+
+        let toml = toml::to_string(&self.config).unwrap();
+
+        file.write_all(toml.as_bytes()).str_result()?;
+
+        file.sync_all().str_result()?;
+
+        Ok(())
+    }
+
+    pub fn expand(&mut self, target_path: String) -> Result<(), String> {
+        Err("Not implemented!".to_string())
+    }
+
+    pub fn mount(&mut self, drive_letter: String) -> Result<(), String> {
         if let Some(expanded_path) = &self.expanded_path {
             match Self::create_volume(&drive_letter, &expanded_path) {
                 true => {
@@ -44,14 +91,14 @@ impl Lab {
 
                     return Ok(())
                 }
-                false => return Err("Failed to create volume!"),
+                false => return Err("Failed to create volume!".to_string()),
             }
         }
 
-        Err("Lab is not expanded!")
+        Err("Lab is not expanded!".to_string())
     }
 
-    pub fn unmount(&mut self) -> Result<(), &'static str> {
+    pub fn unmount(&mut self) -> Result<(), String> {
         if let Some(drive_letter) = &self.drive_letter {
             match Self::delete_volume(&drive_letter) {
                 true => {
@@ -59,11 +106,11 @@ impl Lab {
 
                     return Ok(());
                 }
-                false => return Err("Failed to delete volume!"),
+                false => return Err("Failed to delete volume!".to_string()),
             }
         }
 
-        Err("Lab is not mounted!")
+        Err("Lab is not mounted!".to_string())
     }
 
     #[inline(always)]
@@ -74,5 +121,27 @@ impl Lab {
     #[inline(always)]
     fn delete_volume(drive_letter: &str) -> bool {
         win_subst::del(drive_letter)
+    }
+}
+
+pub trait StrResult<T> {
+    fn str_result(self) -> Result<T, String>;
+}
+
+impl<T> StrResult<T> for Result<T, Error> {
+    fn str_result(self) -> Result<T, String> {
+        match self {
+            Ok(t) => Ok(t),
+            Err(e) => Err(e.to_string())
+        }
+    }
+}
+
+impl<T> StrResult<T> for io::Result<T> {
+    fn str_result(self) -> Result<T, String> {
+        match self {
+            Ok(t) => Ok(t),
+            Err(e) => Err(e.to_string())
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,64 @@
 mod image;
 
-use std::env::args;
+use std::env::{args, Args};
 
 use image::Lab;
 
-fn main() -> Result<(), &'static str> {
+struct RunOptions {
+    exit: bool
+}
+
+fn main() -> Result<(), String> {
     let mut args = args();
     args.next();
 
-    Lab::from_image("path".to_string()).expand("path".to_string())
+    let run_options = parse_args(args)?;
+
+    if run_options.exit {
+        return Ok(());
+    }
+
+    Lab::from_image("test".to_string()).expand("test".to_string())
+}
+
+#[inline(always)]
+fn parse_args(args: Args) -> Result<RunOptions, String> {
+    let mut run_options = RunOptions {
+        exit: false
+    };
+
+    if args.len() == 0 {
+        print_usage();
+
+        run_options.exit = true;
+        return Ok(run_options);
+    }
+
+    for arg in args {
+        if arg.eq("-v") || arg.eq("--version") {
+            print_version();
+
+            run_options.exit = true;
+            return Ok(run_options);
+        } else if arg.eq("-t") || arg.eq("--test") {
+            continue;
+        } else {
+            print_usage();
+
+            run_options.exit = true;
+            return Ok(run_options);
+        }
+    }
+
+    Ok(run_options)
+}
+
+#[inline(always)]
+fn print_version() {
+    println!("Laboratory v0.1.0");
+}
+
+#[inline(always)]
+fn print_usage() {
+    println!("Usage:");
 }
